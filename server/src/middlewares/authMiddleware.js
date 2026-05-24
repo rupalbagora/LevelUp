ee5e8b8fb96bdde26b4141518a77e0c082c555d2
rupalbagora <rupal.bagora@gmail.com>
2026-05-25 01:18:54 +0530

    resolve error

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,30 +1,34 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const protect =  (req,res,next) => {
-  // let token ;
+const protect = (req, res, next) => {
   let token = req.cookies.token;
 
   if (
-    req.headers.authorization && req.headers.authorization.startsWith ("Bearer")
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
-    // console.log(token)
-  }
-  if(!token) {
-    return res.status(401).json({message:"Not authorized, no token"
   }
 
-  try{
-    //  console.log("JWT_SECRET:", process.env.JWT_SECRET);
-    //  console.log("TOKEN:", token);
-    const decoded = jwt.verify(token,process.env.JWT_SECRET);
-    req.user = decoded.id;
-    next();
+  if (!token) {
+    return res.status(401).json({
+      message: "Not authorized, no token",
+    });
   }
-   catch(error){
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.id;
+
+    next();
+  } catch (error) {
     console.error("JWT VERIFY ERROR:", error.message);
-    res.status(401).json({message:"Not authorized, invalid token"});
-   }
-}
+
+    return res.status(401).json({
+      message: "Not authorized, invalid token",
+    });
+  }
+};
 
 export default protect;
