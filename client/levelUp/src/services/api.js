80192e1e2b88273a94b7f5720623434e633c9562
rupalbagora <rupal.bagora@gmail.com>
2026-05-27 19:05:27 +0530

    ..

--- client/levelUp/src/services/api.js
+++ client/levelUp/src/services/api.js
@@ -1,3 +1,13 @@
+// import axios from "axios";
+
+// export const API_BASE = import.meta.env.VITE_API_URL || "";
+
+// const api = axios.create({
+//   baseURL: `${API_BASE}/api`,
+//   withCredentials: true,
+// });
+
+// export default api;
 import axios from "axios";
 
 export const API_BASE = import.meta.env.VITE_API_URL || "";
@@ -6,5 +16,24 @@ const api = axios.create({
   baseURL: `${API_BASE}/api`,
   withCredentials: true,
 });
+
+// Add token automatically to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    config.headers["Cache-Control"] =
+      "no-store, no-cache, must-revalidate, proxy-revalidate";
+
+    config.headers.Expires = "0";
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 export default api;
--- client/levelUp/src/services/banService.js
+++ client/levelUp/src/services/banService.js
@@ -1,9 +1,17 @@
-import axios from "axios";
-const API_URL = `${import.meta.env.VITE_API_URL}/api`; 
-export const getBanStatusAPI = async () => {
-  const response = await axios.get(`${API_URL}/auth/ban-status`, {
-    withCredentials: true,
-  });
+// import axios from "axios";
+// const API_URL = `${import.meta.env.VITE_API_URL}/api`;
+// export const getBanStatusAPI = async () => {
+//   const response = await axios.get(`${API_URL}/auth/ban-status`, {
+//     withCredentials: true,
+//   });
 
-  return response.data;
+//   return response.data;
+// };
+
+import api from "./api.js";
+
+export const getBanStatusAPI = async () => {
+  const { data } = await api.get("/auth/ban-status");
+
+  return data;
 };
